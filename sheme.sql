b580718c7b90e8779a104be89e95ed9c01b50953
Sergey Sokolvyak <zhvkgj@yandex.ru>
2020-11-10 15:54:10 +0000

    add some fixes

--- sheme.sql
+++ sheme.sql
@@ -2,15 +2,14 @@
 -- Id карточек волонтеров начинаются с 1e6 + 1, чтобы не было коллизий с id карточек спортсменов
 -- Волонтёр однозначно определяется номером телефона
 CREATE TABLE Volunteer(
-  card_id INT PRIMARY KEY, 
-  phone TEXT UNIQUE,
-  CHECK (card_id > 1e6)
+  card_id INT PRIMARY KEY CHECK (card_id > 1e6), 
+  phone TEXT NOT NULL UNIQUE
 );
 
 -- Руководитель(имя, PK(контактный телефон))
 -- Руководитель однозначно определяется по номеру телефона.
 CREATE TABLE Leader(
-  name TEXT, 
+  name TEXT NOT NULL, 
   phone TEXT PRIMARY KEY
 );
 
@@ -18,55 +17,64 @@ CREATE TABLE Leader(
 -- Адресс это просто название улицы и номер дома. На адресс ссылаться здание (Building).
 CREATE TABLE Address(
   id SERIAL PRIMARY KEY,
-  street TEXT,
-  house INT
+  street TEXT NOT NULL,
+  house INT NOT NULL CHECK (house > 0)
 );
 
 -- Предназначения(PK(id), предназначение)
 -- Предназначение конкретного объекта на территории деревни
 CREATE TABLE Purpose(
   id INT PRIMARY KEY,
-  purpose TEXT UNIQUE
+  purpose TEXT NOT NULL UNIQUE
 );
 
 -- Объекты(PK(id), FK(id адреса), предназначение, собственное имя)
 -- Подразумевается, что по одному адресу могут располагаться несколько объектов
 CREATE TABLE Building(
   id SERIAL PRIMARY KEY, 
-  address_id INT REFERENCES Address(id), 
-  purpose_id INT REFERENCES Purpose(id),
-  name TEXT
+  address_id INT NOT NULL REFERENCES Address(id), 
+  purpose_id INT NOT NULL REFERENCES Purpose(id),
+  name TEXT 
 );
 
+-- Виды спорта(PK(id), название спорта)
+CREATE TABLE Sports(
+  id INT PRIMARY KEY,
+  name TEXT NOT NULL UNIQUE
+);
+
+-- Ассоциация объектов с видами спорта
+CREATE TABLE Building_Sport(
+  sport_id INT REFERENCES Sports(id),
+  building_id INT REFERENCES Building(id),
+  PRIMARY KEY (sport_id, building_id)
+);
+
+-- Делегация(PK(страна), FK(контактный телефон руководителя), FK(id объекта))
+-- Делегация имеет одного руководителя, который идентифицируется по номеру телефона
+-- У каждой делегации есть один штаб в каком-то здании
+CREATE TABLE Delegation(
+  country TEXT PRIMARY KEY,
+  leader_phone TEXT REFERENCES Leader(phone),
+  building_id INT REFERENCES Building(id)
+);
+
+CREATE TYPE SEX as ENUM ('male', 'female', 'not stated');
 -- Спортсмены(имя, пол, рост, вес, возраст, PK(идентификатор карточки), FK(id объекта))
 -- За каждым спортсменом закреплён волонтёр 
 -- Карточки уникальны и однозначно определяют человека
 -- Номера карточек спортсменов не превышают 1е6, чтобы не было пересечения с волонтёрами
 -- Каждый спортсмен где-то живёт
 CREATE TABLE Sportsman(
-  name TEXT, 
-  gender TEXT, 
-  height INT, 
-  age INT, 
-  card_id INT PRIMARY KEY,
+  name TEXT NOT NULL, 
+  sex SEX NOT NULL DEFAULT 'not stated', 
+  height INT NOT NULL CHECK (height > 0), 
+  weight INT NOT NULL CHECK (weight > 0),
+  age INT NOT NULL CHECK (age > 0), 
+  card_id INT PRIMARY KEY CHECK (card_id <= 1e6),
   volunteer_id INT REFERENCES Volunteer(card_id),
   building_id INT REFERENCES Building(id),
-  CHECK (card_id <= 1e6)
-);
-
--- Делегация(PK(страна), FK(контактный телефон руководителя), FK(id объекта))
--- Делегация имеет одного руководителя, который идентифицируется по номеру телефона
--- У каждой делегации есть один штаб в каком-то здании
-CREATE TABLE DELEGATION(
-  country TEXT PRIMARY KEY,
-  leader_phone TEXT REFERENCES Leader(phone),
-  building_id INT REFERENCES Building(id)
-);
-
--- Виды спорта(PK(id), название спорта)
-CREATE TABLE Sports(
-  id INT PRIMARY KEY,
-  name TEXT UNIQUE
+  delegation_id TEXT REFERENCES Delegation(country)
 );
 
 -- Соревнования(PK(id), вид спорта, дата-время, FK(id объекта))
@@ -74,10 +82,10 @@ CREATE TABLE Sports(
 -- В одно время в одном здании может проводиться только одно соревнование
 CREATE TABLE Competition (
   id INT PRIMARY KEY,
-  sport_id INT REFERENCES Sports(id),
-  competition_date TIMESTAMP,
-  building_id INT REFERENCES Building(id),
-  UNIQUE (competition_date, building_id)
+  sport_id INT NOT NULL,
+  competition_date TIMESTAMP NOT NULL,
+  building_id INT NOT NULL,
+  FOREIGN KEY (sport_id, building_id) REFERENCES Building_Sport(sport_id, building_id)
 );
 
 -- Участники(PK(FK(id спортсмена), FK(id соревнования)), занятое место)
@@ -85,7 +93,7 @@ CREATE TABLE Competition (
 CREATE TABLE Participant(
   sportsman_id INT REFERENCES Sportsman(card_id),
   competition_id INT REFERENCES Competition(id),
-  place TEXT NOT NULL,
+  place INT NOT NULL CHECK (place >= 0),
   PRIMARY KEY (sportsman_id, competition_id)
 );
 
@@ -94,8 +102,7 @@ CREATE TABLE Participant(
 -- Однозначно определяется регистрационным номером
 CREATE TABLE Transport(
   reg_n TEXT PRIMARY KEY,
-  capacity INT,
-  CHECK (capacity > 0)
+  capacity INT NOT NULL CHECK (capacity > 0)
 );
 
 -- Задания(PK(id), FK(id карточнки волонтёра), дата-время, описание)
@@ -104,8 +111,8 @@ CREATE TABLE Transport(
 -- Нельзя, чтобы в одно время к одному транспорту относилось несколько заданий 
 CREATE TABLE Task (
   id INT PRIMARY KEY,
-  volunteer_id INT REFERENCES Volunteer(card_id),
-  task_date TIMESTAMP,
+  volunteer_id INT NOT NULL REFERENCES Volunteer(card_id),
+  task_date TIMESTAMP NOT NULL,
   transport_id TEXT NULL REFERENCES Transport(reg_n),
   task_description TEXT,
   UNIQUE (task_date, transport_id)
