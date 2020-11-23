e2c91a6a7bc1e241431064c2d09b5434514a1bc3
Ekaterina Ledeneva <cthfvrl@gmail.com>
2020-11-23 21:32:54 +0300

    Folder restructuring & template .py's

---
+++ src/webapp.py
@@ -0,0 +1,56 @@
+# encoding: UTF-8
+
+## Веб сервер
+import cherrypy
+
+from connect import parse_cmd_line
+from connect import create_connection
+from static import index
+
+@cherrypy.expose
+class App(object):
+    def __init__(self, args):
+        self.args = args
+
+    @cherrypy.expose
+    def start(self):
+        return "Hello web app"
+
+    @cherrypy.expose
+    def index(self):
+      return index()
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def planets(self, planet_id = None):
+        with create_connection(self.args) as db:
+            cur = db.cursor()
+            if planet_id is None:
+              cur.execute("SELECT id, name FROM Planet P")
+            else:
+              cur.execute("SELECT id, name FROM Planet WHERE id= %s", planet_id)
+            result = []
+            planets = cur.fetchall()
+            for p in planets:
+                result.append({"id": p[0], "name": p[1]})
+            return result
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def commanders(self):
+        with create_connection(self.args) as db:
+            cur = db.cursor()
+            cur.execute("SELECT id, name FROM Commander")
+            result = []
+            commanders = cur.fetchall()
+            for c in commanders:
+                result.append({"id": c[0], "name": c[1]})
+            return result
+
+
+cherrypy.config.update({
+  'server.socket_host': '0.0.0.0',
+  'server.socket_port': 8080,
+})
+cherrypy.quickstart(App(parse_cmd_line()))
+
