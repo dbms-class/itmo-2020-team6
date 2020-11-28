352d4c457d084b9ecb733af227db07e1125793ab
Ekaterina Ledeneva <cthfvrl@gmail.com>
2020-11-28 20:46:25 +0300

    Volunteer load

--- src/webapp.py
+++ src/webapp.py
@@ -56,13 +56,72 @@ class App(object):
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def volunteer_load(self, volunteer_id=None, sportsman_count=0, total_task_count=0):
+    def volunteer_load(self, volunteer_id=None, sportsman_count=-1, total_task_count=-1):
         with create_connection(self.args) as db:
             cur = db.cursor()
-            cur.execute("SELECT V.card_id, V.name, COALESCE(S.sportsman_count, 0) "
-                        "FROM Volunteer V LEFT JOIN (SELECT volunteer_id, COUNT(*) sportsman_count FROM Sportsman GROUP BY volunteer_id) S "
-                        "ON V.card_id = S.volunteer_id"
-                        "WHERE S.sportsman_count >= {sportsman_count};")
+
+            volunteer_filter = f"WHERE V.card_id = {volunteer_id}" if volunteer_id is not None else ""
+
+            query = f"""
+            SELECT  V.card_id,
+                    V.name, 
+                    COALESCE(R.sportsman_count, 0),
+                    COALESCE(R.total_task_count, 0),
+                    R.next_task_id,
+                    R.next_task_time::TEXT
+            FROM Volunteer V LEFT JOIN
+            (
+                SELECT  COALESCE(S.id, T.id)            id, 
+                        COALESCE(S.sportsman_count, 0)  sportsman_count,
+                        COALESCE(T.total_task_count, 0) total_task_count, 
+                        T.next_task_id                  next_task_id, 
+                        T.next_task_time                next_task_time
+                FROM
+                (
+                    SELECT  volunteer_id    id, 
+                            COUNT(*)        sportsman_count 
+                    FROM Sportsman 
+                    GROUP BY volunteer_id
+                ) S
+                FULL JOIN
+                (
+                    WITH T3 AS
+                    (
+                        SELECT  T2.volunteer_id      id,
+                                T2.total_task_count  total_task_count,
+                                T2.next_task_time    next_task_time,
+                                T1.id                next_task_id
+                        FROM Task T1 
+                        JOIN 
+                        (
+                            SELECT  volunteer_id, 
+                                    COUNT(*)        total_task_count, 
+                                    MIN(task_date)  next_task_time
+                            FROM Task 
+                            WHERE task_date >= now()
+                            GROUP BY volunteer_id 
+                        ) T2
+                        ON  T1.volunteer_id = T2.volunteer_id AND
+                            T2.next_task_time = T1.task_date
+                    )
+                    SELECT * 
+                    FROM T3
+                    WHERE T3.next_task_id IN (
+                        SELECT MIN(next_task_id)
+                        FROM T3
+                        GROUP BY id
+                    )
+                ) T
+                ON S.id = T.id
+                WHERE   COALESCE(S.sportsman_count, 0) >= {sportsman_count} AND
+                        COALESCE(T.total_task_count, 0) >= {total_task_count}
+            ) R
+            ON V.card_id = R.id
+            {volunteer_filter}
+            ;
+            """
+
+            cur.execute(query)
             volunteers = cur.fetchall()
             return [{"volunteer_id": v[0],
                      "volunteer_name": v[1],
