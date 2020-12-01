d02db9c6c7e0c20808ef9eb3c6b3d77af9eec8f8
sloboegen98 <sloboegen98@ya.ru>
2020-12-01 18:03:29 +0300

    try to add pool connection

--- src/webapp.py
+++ src/webapp.py
@@ -2,6 +2,7 @@
 
 # Веб сервер
 import cherrypy
+import psycopg2
 
 from connect import parse_cmd_line
 from connect import create_connection
@@ -12,6 +13,15 @@ from static import index
 class App(object):
     def __init__(self, args):
         self.args = args
+        self.postgreSQL_pool = psycopg2.pool.SimpleConnectionPool(1, 20,
+                user="postgres",
+                password="postgres",
+                host="127.0.0.1",
+                post="5432",
+                database="")
+
+        if (self.postgreSQL_pool):
+            print("Successfully connected")
 
     @cherrypy.expose
     def start(self):
@@ -24,8 +34,10 @@ class App(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def register(self, sportsman, country, volunteer_id):
-        with create_connection(self.args) as db:
-            cur = db.cursor()
+        db_connection = self.postgreSQL_pool.getconn()
+
+        if (db_connection):
+            cur = db_connection.cursor()
             if sportsman.isnumeric():
                 cur.execute(f"UPDATE Sportsman SET delegation_id = '{country}', "
                             f"volunteer_id = {volunteer_id} "
@@ -34,53 +46,73 @@ class App(object):
                 cur.execute(f"INSERT INTO Sportsman (name, volunteer_id, delegation_id) "
                             f"VALUES ('{sportsman}', {volunteer_id}, '{country}');")
 
+            cur.close()
+            self.postgreSQL_pool.putconn(db_connection)
+
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def countries(self):
-        with create_connection(self.args) as db:
-            cur = db.cursor()
+        # with create_connection(self.args) as db:
+        db_connection = self.postgreSQL_pool.getconn()
+
+        if (db_connection):
+            cur = db_connection.cursor()
             cur.execute("SELECT country FROM Delegation")
             countries = cur.fetchall()
+
+            cur.close()
+            self.postgreSQL_pool.putconn(db_connection)
+
             return [{"id": i + 1, "name": c[0]}
                     for i, c in enumerate(countries)]
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def volunteers(self):
-        with create_connection(self.args) as db:
-            cur = db.cursor()
+        db_connection = self.postgreSQL_pool.getconn()
+
+        if (db_connection):
+            cur = db_connection.cursor()
             cur.execute("SELECT card_id, name FROM Volunteer")
             volunteers = cur.fetchall()
+
+            cur.close()
+            self.postgreSQL_pool.putconn(db_connection)
+
             return [{"id": v[0], "name": v[1]}
                     for v in volunteers]
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def volunteer_load(self, volunteer_id=None, sportsman_count=-1, total_task_count=-1):
-        with create_connection(self.args) as db:
-            cur = db.cursor()
+        # with create_connection(self.args) as db:
+
+        db_connection = self.postgreSQL_pool.getconn()
+
+        if (db_connection):
+            cur = db_connection.cursor()
 
             volunteer_filter = f"WHERE V.card_id = {volunteer_id}" if volunteer_id is not None else ""
 
             query = f"""
             SELECT  V.card_id,
-                    V.name, 
+                    V.name,
                     COALESCE(R.sportsman_count, 0),
                     COALESCE(R.total_task_count, 0),
                     R.next_task_id,
                     R.next_task_time::TEXT
             FROM Volunteer V LEFT JOIN
             (
-                SELECT  COALESCE(S.id, T.id)            id, 
+                SELECT  COALESCE(S.id, T.id)            id,
                         COALESCE(S.sportsman_count, 0)  sportsman_count,
-                        COALESCE(T.total_task_count, 0) total_task_count, 
-                        T.next_task_id                  next_task_id, 
+                        COALESCE(T.total_task_count, 0) total_task_count,
+                        T.next_task_id                  next_task_id,
                         T.next_task_time                next_task_time
                 FROM
                 (
-                    SELECT  volunteer_id    id, 
-                            COUNT(*)        sportsman_count 
-                    FROM Sportsman 
+                    SELECT  volunteer_id    id,
+                            COUNT(*)        sportsman_count
+                    FROM Sportsman
                     GROUP BY volunteer_id
                 ) S
                 FULL JOIN
@@ -91,20 +123,20 @@ class App(object):
                                 T2.total_task_count  total_task_count,
                                 T2.next_task_time    next_task_time,
                                 T1.id                next_task_id
-                        FROM Task T1 
-                        JOIN 
+                        FROM Task T1
+                        JOIN
                         (
-                            SELECT  volunteer_id, 
-                                    COUNT(*)        total_task_count, 
+                            SELECT  volunteer_id,
+                                    COUNT(*)        total_task_count,
                                     MIN(task_date)  next_task_time
-                            FROM Task 
+                            FROM Task
                             WHERE task_date >= now()
-                            GROUP BY volunteer_id 
+                            GROUP BY volunteer_id
                         ) T2
                         ON  T1.volunteer_id = T2.volunteer_id AND
                             T2.next_task_time = T1.task_date
                     )
-                    SELECT * 
+                    SELECT *
                     FROM T3
                     WHERE T3.next_task_id IN (
                         SELECT MIN(next_task_id)
@@ -123,6 +155,10 @@ class App(object):
 
             cur.execute(query)
             volunteers = cur.fetchall()
+
+            cur.close()
+            self.postgreSQL_pool.putconn(db_connection)
+
             return [{"volunteer_id": v[0],
                      "volunteer_name": v[1],
                      "sportsman_count": v[2],
