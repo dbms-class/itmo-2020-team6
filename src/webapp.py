6d5c6867d7efcb30186d53cf911ad950f3b904b7
sloboegen98 <sloboegen98@ya.ru>
2020-12-08 18:30:32 +0300

    major fix for task 5

--- src/webapp.py
+++ src/webapp.py
@@ -64,23 +64,23 @@ class App(object):
 
             query = f"""
             SELECT  V.card_id,
-                    V.name, 
+                    V.name,
                     COALESCE(R.sportsman_count, 0),
                     COALESCE(R.total_task_count, 0),
                     R.next_task_id,
                     R.next_task_time::TEXT
             FROM Volunteer V LEFT JOIN
             (
-                SELECT  COALESCE(S.id, T.id)            id, 
+                SELECT  COALESCE(S.id, T.id)            id,
                         COALESCE(S.sportsman_count, 0)  sportsman_count,
-                        COALESCE(T.total_task_count, 0) total_task_count, 
-                        T.next_task_id                  next_task_id, 
+                        COALESCE(T.total_task_count, 0) total_task_count,
+                        T.next_task_id                  next_task_id,
                         T.next_task_time                next_task_time
                 FROM
                 (
-                    SELECT  volunteer_id    id, 
-                            COUNT(*)        sportsman_count 
-                    FROM Sportsman 
+                    SELECT  volunteer_id    id,
+                            COUNT(*)        sportsman_count
+                    FROM Sportsman
                     GROUP BY volunteer_id
                 ) S
                 FULL JOIN
@@ -89,15 +89,15 @@ class App(object):
                             T2.total_task_count  total_task_count,
                             T2.next_task_time    next_task_time,
                             T1.id                next_task_id
-                    FROM Task T1 
-                    JOIN 
+                    FROM Task T1
+                    JOIN
                     (
-                        SELECT  volunteer_id, 
-                                COUNT(*)        total_task_count, 
+                        SELECT  volunteer_id,
+                                COUNT(*)        total_task_count,
                                 MIN(task_date)  next_task_time
-                        FROM Task 
+                        FROM Task
                         WHERE task_date >= now()
-                        GROUP BY volunteer_id 
+                        GROUP BY volunteer_id
                     ) T2
                     ON  T1.volunteer_id = T2.volunteer_id AND
                         T2.next_task_time = T1.task_date
@@ -120,6 +120,78 @@ class App(object):
                      "next_task_id": v[4],
                      "next_task_time": v[5]}
                     for v in volunteers]
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def volunteer_unassigned(self, volunteer_id, task_ids):
+        # TODO
+        # 1. convert tuples for sql
+        # 2. *
+        # 3. volunteer's name
+
+        with create_connection(self.args) as db:
+            cur = db.cursor()
+
+            tasks = tuple(map(int, task_ids.split(',')))
+
+            query_volunteers = f'''WITH (SELECT delegation_id
+                                        FROM Sportsman
+                                        WHERE volunteer_id = {volunteer_id}) as PD
+                                SELECT DISTINCT volunteer_id
+                                FROM Sportsman
+                                WHERE delegation_id IN PD;'''
+
+            cur.execute(query_volunteers)
+            possible_volunteers = cur.fetchall()
+
+            query_sorted_tasks = f'''SELECT id, task_date
+                                     FROM Task
+                                     WHERE id IN {str(tasks)}
+                                     ORDER BY task_date;'''
+
+            cur.execute(query_sorted_tasks)
+            sorted_tasks = cur.fetchall()
+
+            volunteer_assigners = []
+
+            for task_id, task_date in sorted_tasks:
+                query_possible_changers = f'''
+                SELECT volunteer_id
+                FROM Volunteers
+                WHERE volunteer_id IN {str(possible_volunteers)}
+                    AND NOT EXISTS (SELECT task_date
+                                    FROM Task
+                                    WHERE task_date BETWEEN {task_date} - interval '1 hour'
+                                                        AND {task_date} + interval '1 hour'
+                                            AND Task.volunteer_id = Volunteers.volunteer_id);'''
+
+                cur.execute(query_possible_changers)
+                possible_changers = cur.fetchall()
+
+                query_sort_by_task_count = f'''
+                SELECT volunteer_id, COUNT(*) as cnt
+                FROM Task
+                WHERE vounteer_id IN {str(possible_changers)}
+                      AND task_date > {task_date}
+                GROUP BY volunteer_id
+                ORDER BY cnt;'''
+
+                cur.execute(query_sort_by_task_count)
+                possible_changers = cur.fetchall()
+
+                if possible_changers:
+                    assigner = possible_changers[0][0]
+
+                    query_update = f'''
+                    UPDATE Task
+                    SET volunteer_id = {assigner}
+                    WHERE id = {task_id};'''
+
+                    cur.execute(query_update)
+
+                    volunteer_assigners.append({'task_id': task_id, 'new_volunteer_name': None, 'new_volnteer_id': assigner})
+
+        return volunteer_assigners
 
 
 cherrypy.config.update({
